4be0026e59ffe59a561faa26fdac4787b32f7966
rai <am@aarai.com>
2014-04-27 21:43:50 -0400

    Add new example

--- example/html/js/main.js
+++ example/html/js/main.js
@@ -1,2 +1,3 @@
 
-$('#img-carousel').videoCarousel();
+$('#img-carousel').videoCarousel({count: 3});
+$('#img-carousel1').videoCarousel({count: 4});
--- example/html/js/plugins.js
+++ example/html/js/plugins.js
@@ -23,60 +23,139 @@
 
 // Place any jQuery/helper plugins in here.
 $.fn.videoCarousel = function (options) {
-   var imgs = this.find('img'), state = $('#img-carousel');
+   var imgs = this.find('img'), state, settings;
 
-   var settings = $.extend({
-       prev: '.left',
-       next: '.right',
-       centerImgClass: 'main',
-       minWidth: '150px',
-       maxWidth: '368px',
-       left: 0,
-       center: '22%',
-       right: '76%',
-       timing: 500,
-   }, options);
+   if (options.count === 3) {
+      settings = $.extend({
+         prev: '.left',
+         next: '.right',
+         centerImgClass: 'main',
+         minWidth: '150px',
+         maxWidth: '368px',
+         left: 0,
+         center: '22%',
+         right: '76%',
+         timing: 500,
+     }, options);
+     state = $('#img-carousel')
 
-   $(settings.prev).click(function () {
-      if (state.attr('data-state')==="one") {
-        imgs.eq(0).animate({left: settings.right, width: settings.minWidth});
-        imgs.eq(1).animate({left: settings.left, width: settings.minWidth})
-        .removeClass(settings.centerImgClass);
-        imgs.eq(2).animate({left: settings.center, width: settings.maxWidth})
-        .addClass(settings.centerImgClass);
-        state.attr('data-state','two');
-      } else if (state.attr('data-state')==="two") {
-        imgs.eq(0).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
-        imgs.eq(1).animate({left: settings.right, width: settings.minWidth});
-        imgs.eq(2).animate({left: settings.left, width: settings.minWidth}).removeClass(settings.centerImgClass);
-        state.attr('data-state','three');
-      } else {
-        imgs.eq(0).animate({left: 0, width: settings.minWidth}).removeClass(settings.centerImgClass);
-        imgs.eq(1).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
-        imgs.eq(2).animate({left: settings.right, width: settings.minWidth});
-        state.attr('data-state','one');
-      }
+     $(settings.prev).click(function () {
+        if (state.attr('data-state')==="one") {
+          imgs.eq(0).animate({left: settings.right, width: settings.minWidth});
+          imgs.eq(1).animate({left: settings.left, width: settings.minWidth})
+          .removeClass(settings.centerImgClass);
+          imgs.eq(2).animate({left: settings.center, width: settings.maxWidth})
+          .addClass(settings.centerImgClass);
+          state.attr('data-state','two');
+        } else if (state.attr('data-state')==="two") {
+          imgs.eq(0).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
+          imgs.eq(1).animate({left: settings.right, width: settings.minWidth});
+          imgs.eq(2).animate({left: settings.left, width: settings.minWidth}).removeClass(settings.centerImgClass);
+          state.attr('data-state','three');
+        } else {
+          imgs.eq(0).animate({left: 0, width: settings.minWidth}).removeClass(settings.centerImgClass);
+          imgs.eq(1).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
+          imgs.eq(2).animate({left: settings.right, width: settings.minWidth});
+          state.attr('data-state','one');
+        }
+        return false;
+     });
 
-   });
+     $(settings.next).click(function () {
+        if (state.attr('data-state')==="one") {
+          imgs.eq(0).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
+          imgs.eq(1).animate({left: settings.right, width: settings.minWidth}).removeClass(settings.centerImgClass);
+          imgs.eq(2).animate({left: settings.left, width: settings.minWidth});
+          state.attr('data-state','three');
+        } else if (state.attr('data-state')==="two") {
+          imgs.eq(0).animate({left: settings.left, width: settings.minWidth});
+          imgs.eq(1).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
+          imgs.eq(2).animate({left: settings.right, width: settings.minWidth}).removeClass(settings.centerImgClass);
+          state.attr('data-state','one');
+        } else {
+          imgs.eq(0).animate({left: settings.right, width: settings.minWidth}).removeClass(settings.centerImgClass);
+          imgs.eq(1).animate({left: settings.left, width: settings.minWidth});
+          imgs.eq(2).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
+          state.attr('data-state','two');
+        }
+        return false;
+     });
 
-   $(settings.next).click(function () {
-      if (state.attr('data-state')==="one") {
-        imgs.eq(0).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
-        imgs.eq(1).animate({left: settings.right, width: settings.minWidth}).removeClass(settings.centerImgClass);
-        imgs.eq(2).animate({left: settings.left, width: settings.minWidth});
-        state.attr('data-state','three');
-      } else if (state.attr('data-state')==="two") {
-        imgs.eq(0).animate({left: settings.left, width: settings.minWidth});
-        imgs.eq(1).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
-        imgs.eq(2).animate({left: settings.right, width: settings.minWidth}).removeClass(settings.centerImgClass);
-        state.attr('data-state','one');
-      } else {
-        imgs.eq(0).animate({left: settings.right, width: settings.minWidth}).removeClass(settings.centerImgClass);
-        imgs.eq(1).animate({left: settings.left, width: settings.minWidth});
-        imgs.eq(2).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
-        state.attr('data-state','two');
-      }
-   });
+   } else if(options.count === 4) {
+      settings = $.extend({
+         prev: '.left1',
+         next: '.right1',
+         centerImgClass: 'main',
+         minWidth: '150px',
+         maxWidth: '368px',
+         left: 0,
+         center: '22%',
+         back: '38%',
+         right: '76%',
+         timing: 500,
+     }, options);
+     state = $('#img-carousel1')
+
+     $(settings.prev).click(function () {
+        if (state.attr('data-state')==="one") {
+          imgs.eq(0).animate({left: settings.back, width: settings.minWidth}).css('z-index','-1');
+          imgs.eq(1).animate({left: settings.left, width: settings.minWidth}).removeClass(settings.centerImgClass);
+          imgs.eq(2).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
+          imgs.eq(3).animate({left: settings.right, width: settings.minWidth}).css('z-index','0');
+          state.attr('data-state','four');
+        } else if (state.attr('data-state')==="two") {
+          imgs.eq(0).animate({left: settings.right, width: settings.minWidth}).css('z-index','0');
+          imgs.eq(1).animate({left: settings.back, width: settings.minWidth}).css('z-index','-1');
+          imgs.eq(2).animate({left: settings.left, width: settings.minWidth}).removeClass(settings.centerImgClass);
+          imgs.eq(3).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
+          state.attr('data-state','three');
+        } else if (state.attr('data-state')==="three") {
+          imgs.eq(0).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
+          imgs.eq(1).animate({left: settings.right, width: settings.minWidth}).css('z-index','0');
+          imgs.eq(2).animate({left: settings.back, width: settings.minWidth}).css('z-index','-1');
+          imgs.eq(3).animate({left: settings.left, width: settings.minWidth}).removeClass(settings.centerImgClass);
+          state.attr('data-state','four');
+        } else {
+          imgs.eq(0).animate({left: settings.left, width: settings.minWidth}).removeClass(settings.centerImgClass);
+          imgs.eq(1).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
+          imgs.eq(2).animate({left: settings.right, width: settings.minWidth}).css('z-index','0');
+          imgs.eq(3).animate({left: settings.back, width: settings.minWidth}).css('z-index','-1');
+          state.attr('data-state','one');
+        }
+        return false;
+     });
+
+     $(settings.next).click(function () {
+        if (state.attr('data-state')==="one") {
+          imgs.eq(0).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
+          imgs.eq(1).animate({left: settings.right, width: settings.minWidth}).removeClass(settings.centerImgClass);
+          imgs.eq(2).animate({left: settings.back, width: settings.minWidth}).css('z-index','-1');
+          imgs.eq(3).animate({left: settings.left, width: settings.minWidth});
+          state.attr('data-state','two');
+        } else if (state.attr('data-state')==="two") {
+          imgs.eq(0).animate({left: settings.right, width: settings.minWidth}).removeClass(settings.centerImgClass);
+          imgs.eq(1).animate({left: settings.back, width: settings.minWidth}).css('z-index','-1');
+          imgs.eq(2).animate({left: settings.left, width: settings.minWidth}).css('z-index','0');
+          imgs.eq(3).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass).css('z-index','0');
+          state.attr('data-state','three');
+        } else if (state.attr('data-state')==="three") {
+          imgs.eq(0).animate({left: settings.back, width: settings.minWidth}).css('z-index','-1');
+          imgs.eq(1).animate({left: settings.left, width: settings.minWidth}).css('z-index','0');
+          imgs.eq(2).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
+          imgs.eq(3).animate({left: settings.right, width: settings.minWidth}).removeClass(settings.centerImgClass);
+          state.attr('data-state','four');
+        } else {
+          imgs.eq(0).animate({left: settings.left, width: settings.minWidth}).css('z-index','0');
+          imgs.eq(1).animate({left: settings.center, width: settings.maxWidth}).addClass(settings.centerImgClass);
+          imgs.eq(2).animate({left: settings.right, width: settings.minWidth}).removeClass(settings.centerImgClass);
+          imgs.eq(3).animate({left: settings.back, width: settings.minWidth}).css('z-index','-1');
+          state.attr('data-state','one');
+        }
+        return false;
+     });
+   } else {
+      throw "Invalid image count: " + options.count;
+   }
+
    return this;
 };
-
